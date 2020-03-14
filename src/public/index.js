5be87070c78a9ea62404c0723072213007fdce4d
Vlad Shamaev <vladshamaev@gmail.com>
2020-03-14 17:28:43 +0300

    Компоненты магазина работают. Не работает сборка.

---
+++ src/public/index.js
@@ -0,0 +1,9 @@
+import '@babel/polyfill'
+import './css/style.css'
+import appMain from './js/main'
+import './js/ProdComp.js'
+import './js/CartComp.js'
+import './js/main.js'
+import './js/vue.js'
+
+const app = new Vue(appMain);
